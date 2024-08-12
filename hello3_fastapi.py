268d8837001783f4e9968d75c02c4236828007fd
sun12wook11 <sun12wook11@gmail.com>
2024-08-12 17:34:47 +0900

    경로 매개변수

--- hello3_fastapi.py
+++ hello3_fastapi.py
@@ -4,11 +4,16 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def index():
-
     return "Hello World, again!!"
+@app.get("/sayHello")
+def sayhello(msg :str):
+    return f'Hello, {msg}!!'
+@app.get("/sayAgain/{msg}")
+def sayagain(msg :str):
+    return f'Hello, {msg}!!'
 
 # __name__: 실행중인 모듈이름을 의미하는 매직키워드
 # 만일, 파일을 직접 실행하면 __name__의 이름은 __main__으로 자동지정
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('hello2_fastapi:app', reload=True)
+    uvicorn.run('hello3_fastapi:app', reload=True)
